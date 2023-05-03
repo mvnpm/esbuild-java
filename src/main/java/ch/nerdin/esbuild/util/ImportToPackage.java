7e76c0ec0830e73bcc4399aa190f6a8b6c04eac9
Andy Damevin <ia3andy@gmail.com>
2023-05-03 17:17:40 +0200

    Support for packages with index for subpath import

--- src/main/java/ch/nerdin/esbuild/util/ImportToPackage.java
+++ src/main/java/ch/nerdin/esbuild/util/ImportToPackage.java
@@ -8,10 +8,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ImportToPackage {
     private static final String MVNPM_PACKAGE_PREFIX = "resources";
@@ -21,56 +21,65 @@ public class ImportToPackage {
         return QuteTemplateRenderer.render("package-template.json", Map.of("name", name, "version", version, "main", main));
     }
 
-    protected static PackageInfo extractInfo(Path importMapFile) {
+    protected static List<PackageInfo> extractPackages(Path importMapFile) {
         try {
             final JSONTokener jsonTokener = new JSONTokener(new FileInputStream(importMapFile.toFile()));
             final JSONObject object = new JSONObject(jsonTokener);
 
             final JSONObject imports = object.getJSONObject("imports");
             final Iterator<String> keys = imports.keys();
-            String name = null;
-            String dir = null;
-            String main = null;
+            final List<PackageInfo> packages = new ArrayList<>();
             while (keys.hasNext()) {
                 String key = keys.next();
                 final String value = imports.getString(key);
-                if (value.endsWith(".js") || value.endsWith(".mjs")) {
-                    name = key;
-                    main = value;
-                } else if (value.endsWith("/")) {
-                    dir = value;
+                if (value.endsWith(".js") || value.endsWith(".mjs") && imports.has(key + "/")) {
+                    final String dir;
+                    if ((value.endsWith("index.js") || value.endsWith(".mjs"))
+                        && !hasScripts(importMapFile.getParent().resolve(MVNPM_PACKAGE_PREFIX + imports.getString(key + "/")))) {
+                        // This is an index and the importmap dir does not contain any script
+                        // use the index parent directory as package root
+                        dir = value.substring(0, value.lastIndexOf("/") + 1);
+                    } else {
+                        dir = imports.getString(key + "/");
+                    }
+                    packages.add(new PackageInfo(key, value.substring(dir.length()), dir));
                 }
             }
-            if(name == null || dir == null || main == null) {
-                throw new RuntimeException("could not find script in import map");
-            }
-            return new PackageInfo(name, main.substring(dir.length(), main.length()), dir);
-        } catch (FileNotFoundException e) {
+            return packages;
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean hasScripts(Path dir) throws IOException {
+        return Files.list(dir)
+            .map(Path::getFileName)
+            .map(Path::toString)
+            .anyMatch(n -> n.endsWith(".js") || n.endsWith(".mjs") || n.endsWith(".css"));
     }
 
 
     public static void createPackage(Path nodeModules, Path location, String moduleName, String version) throws IOException {
         final Path importMapFile = location.resolve(IMPORT_FILE_NAME);
-        if(!Files.isRegularFile(importMapFile)) {
+        if (!Files.isRegularFile(importMapFile)) {
             // This is not a valid MVNPM dependency
             return;
         }
-        final PackageInfo info = extractInfo(importMapFile);
-        final String packageContents = convert(info.getName(), version,  info.getMain());
-        final Path packageDir = importMapFile.getParent().resolve(MVNPM_PACKAGE_PREFIX + info.getDirectory());
-        if(!Files.isDirectory(packageDir)) {
-            throw new IllegalStateException("Invalid MVNPM dependency structure: " + packageDir);
+        final List<PackageInfo> packages = extractPackages(importMapFile);
+        for (PackageInfo info : packages) {
+            final String packageContents = convert(info.getName(), version, info.getMain());
+            final Path packageDir = importMapFile.getParent().resolve(MVNPM_PACKAGE_PREFIX + info.getDirectory());
+            if (!Files.isDirectory(packageDir)) {
+                throw new IllegalStateException("Invalid MVNPM dependency structure: " + packageDir);
+            }
+            final Path packageFile = packageDir.resolve("package.json");
+            Files.writeString(packageFile, packageContents);
+
+            // in case there is a / in the name
+            final Path targetDir = nodeModules.resolve(info.getName());
+            if (!Files.exists(targetDir.getParent())) Files.createDirectories(targetDir.getParent());
+            Files.move(packageFile.getParent(), targetDir);
         }
-        final Path packageFile = packageDir.resolve("package.json");
-        Files.writeString(packageFile, packageContents);
-
-        // in case there is a / in the name
-        final Path targetDir = nodeModules.resolve(info.getName());
-        if (!Files.exists(targetDir.getParent())) Files.createDirectories(targetDir.getParent());
-        Files.move(packageFile.getParent(), targetDir);
-
     }
 
     static class PackageInfo {
