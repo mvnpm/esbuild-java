04f5ac2ba3e3c2ee945335a469a354619d07dd42
Phillip Kruger <phillip.kruger@gmail.com>
2023-08-31 13:19:23 +1000

    Add support for composite mvnpm jars
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- src/test/java/io/mvnpm/esbuild/util/PackageJsonTest.java
+++ src/test/java/io/mvnpm/esbuild/util/PackageJsonTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,19 +17,28 @@ class PackageJsonTest {
     void findPackageMVNPMTest() throws URISyntaxException, IOException {
         final Path temp = Files.createTempDirectory("findPackageMVNPMTest");
         UnZip.unzip(new File(getClass().getResource("/mvnpm/react-bootstrap-2.7.4.jar").toURI()).toPath(), temp);
-        final Optional<Path> packageJson = PackageJson.findPackageJson(temp);
-        assertTrue(packageJson.isPresent());
-        final String name = PackageJson.readPackageName(packageJson.get());
+        final List<Path> packageJson = JarInspector.findPackageJsons(temp, false);
+        assertFalse(packageJson.isEmpty());
+        final String name = JarInspector.readPackageName(packageJson.get(0));
         assertEquals("react-bootstrap", name);
     }
 
     @Test
+    void findPackageMVNPMCompositeTest() throws URISyntaxException, IOException {
+        final Path temp = Files.createTempDirectory("findPackageMVNPMCompositeTest");
+        UnZip.unzip(new File(getClass().getResource("/mvnpm/vaadin-webcomponents-24.1.6.jar").toURI()).toPath(), temp);
+        final List<Path> packageJsons = JarInspector.findPackageJsons(temp, true);
+        assertFalse(packageJsons.isEmpty());
+        assertTrue(packageJsons.size()>1);
+    }
+    
+    @Test
     void findPackageWebjarsTest() throws URISyntaxException, IOException {
         final Path temp = Files.createTempDirectory("findPackageWebjarsTest");
         UnZip.unzip(new File(getClass().getResource("/webjars/restart__hooks-0.4.9.jar").toURI()).toPath(), temp);
-        final Optional<Path> packageJson = PackageJson.findPackageJson(temp);
-        assertTrue(packageJson.isPresent());
-        final String name = PackageJson.readPackageName(packageJson.get());
+        final List<Path> packageJson = JarInspector.findPackageJsons(temp, false);
+        assertFalse(packageJson.isEmpty());
+        final String name = JarInspector.readPackageName(packageJson.get(0));
         assertEquals("@restart/hooks", name);
     }
 }
