47bf4b7bb3933151c1f3ee5f911f02ffdfd351c9
Erik Jan de Wit <erikjan.dewit@gmail.com>
2023-09-27 08:14:23 +0200

    fixed the main output

--- src/main/java/io/mvnpm/esbuild/Main.java
+++ src/main/java/io/mvnpm/esbuild/Main.java
@@ -1,5 +1,6 @@
 package io.mvnpm.esbuild;
 
+import io.mvnpm.esbuild.model.ExecuteResult;
 import io.mvnpm.esbuild.resolve.ExecutableResolver;
 
 import java.io.IOException;
@@ -9,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         final Path esBuildExec = new ExecutableResolver().resolve(Bundler.getDefaultVersion());
-        new Execute(esBuildExec.toFile(), args).executeAndWait();
+        final ExecuteResult executeResult = new Execute(esBuildExec.toFile(), args).executeAndWait();
+        System.out.println(executeResult.output());
     }
 }
