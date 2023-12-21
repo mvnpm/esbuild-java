a6f77a1e8a212672946d5b64fdffab840e04e244
Andy Damevin <ia3andy@gmail.com>
2023-12-21 11:10:38 +0100

    Use workdir for process

--- src/main/java/io/mvnpm/esbuild/Main.java
+++ src/main/java/io/mvnpm/esbuild/Main.java
@@ -2,6 +2,7 @@ package io.mvnpm.esbuild;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import io.mvnpm.esbuild.model.ExecuteResult;
 import io.mvnpm.esbuild.resolve.ExecutableResolver;
@@ -9,8 +10,10 @@ import io.mvnpm.esbuild.resolve.ExecutableResolver;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        String workingDirectory = System.getProperty("user.dir");
         final Path esBuildExec = new ExecutableResolver().resolve(Bundler.ESBUILD_EMBEDDED_VERSION);
-        final ExecuteResult executeResult = new Execute(esBuildExec.toFile(), args).executeAndWait();
+        final ExecuteResult executeResult = new Execute(Paths.get(workingDirectory), esBuildExec.toFile(), args)
+                .executeAndWait();
         System.out.println(executeResult.output());
     }
 }
