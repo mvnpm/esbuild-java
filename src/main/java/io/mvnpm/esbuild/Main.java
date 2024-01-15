4e450437a919badefa0524f2d3584168f6e597aa
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-01-15 15:01:21 +0100

    added esbuild version to the config
    fixes: #88

--- src/main/java/io/mvnpm/esbuild/Main.java
+++ src/main/java/io/mvnpm/esbuild/Main.java
@@ -3,6 +3,10 @@ package io.mvnpm.esbuild;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import io.mvnpm.esbuild.model.ExecuteResult;
 import io.mvnpm.esbuild.resolve.ExecutableResolver;
@@ -11,8 +15,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String workingDirectory = System.getProperty("user.dir");
-        final Path esBuildExec = new ExecutableResolver().resolve(Bundler.ESBUILD_EMBEDDED_VERSION);
-        final ExecuteResult executeResult = new Execute(Paths.get(workingDirectory), esBuildExec.toFile(), args)
+        final List<String> arguments = new ArrayList<>(Arrays.asList(args));
+        final Optional<String> esbuildVersionArgument = arguments.stream().filter(a -> a.startsWith("--esbuildVersion"))
+                .findFirst();
+        String esbuildVersion = Bundler.ESBUILD_EMBEDDED_VERSION;
+        if (esbuildVersionArgument.isPresent()) {
+            arguments.remove(esbuildVersionArgument.get());
+            final String unParsedEsbuildVersion = esbuildVersionArgument.get();
+            esbuildVersion = unParsedEsbuildVersion.split("=")[1];
+        }
+        final Path esBuildExec = new ExecutableResolver().resolve(esbuildVersion);
+        final ExecuteResult executeResult = new Execute(Paths.get(workingDirectory), esBuildExec.toFile(),
+                arguments.toArray(new String[] {}))
                 .executeAndWait();
         System.out.println(executeResult.output());
     }
